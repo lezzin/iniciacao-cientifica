b3a8d782ff2447970f18c57800592b5ca2ae41b6
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2023-07-01 18:20:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// 
+"use strict";
+
+var header = document.querySelector(".header.dynamic");
+
+if (header) {
+    addEventListener("scroll", () => {
+        (scrollY > 330) ? header.classList.add("active") : header.classList.remove("active");
+    })
+}
+
+var btn = document.querySelector("#btn");
+
+if (btn) {
+    var menu = document.querySelector("nav");
+    btn.addEventListener("click", () => { menu.classList.toggle("active") })
+}
