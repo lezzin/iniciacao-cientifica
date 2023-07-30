2ab1a89b76bc8101faca47a513ca27dd23124033
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2023-07-30 20:06:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 var header = document.querySelector(".header.dynamic");
 
 if (header) {
-    var currentPagesHme = location.href.includes("index.html");
-    var number = currentPagesHme ? 400 : 150;
+    var currentPageHome = location.href.includes("index.html") || !location.href.includes("html");
+    var number = currentPageHome ? 400 : 150;
 
     addEventListener("scroll", () => {
         (scrollY > number) ? header.classList.add("active") : header.classList.remove("active");
